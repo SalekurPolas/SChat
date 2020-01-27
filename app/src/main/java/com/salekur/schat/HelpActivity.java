2907478d24cd6e73bc19591f5b8b064fc1372c91
Salekur Rahaman <salekur19@gmail.com>
2020-01-27 21:56:38 +0600

    Update on HelpActivity:
    Add comment: //nothing to do

--- app/src/main/java/com/salekur/schat/HelpActivity.java
+++ app/src/main/java/com/salekur/schat/HelpActivity.java
@@ -13,5 +13,7 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        //nothing to do
     }
 }
